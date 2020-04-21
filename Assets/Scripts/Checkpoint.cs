ba31466d50033bb3690b658bb625011e29c3122b
johaker <akerfeldtjohan@gmail.com>
2020-04-21 13:46:26 +0200

    Respawn
    Respawn

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Transform checkpoint;
+    GameObject player;
+
+    public void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider plyr)
+    {
+        if(plyr.gameObject.tag == "Player")
+        {
+            player.transform.position = checkpoint.position;
+            player.transform.rotation = checkpoint.rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform player;
+    public Transform checkpoint;
+    public Rigidbody rb;
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = checkpoint.transform.position;
+        player.transform.rotation = checkpoint.rotation;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+}
