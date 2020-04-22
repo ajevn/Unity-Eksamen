dce55af75013bdb5dfbf83bf704eba4a4e2853ee
Andreas Jevnaker <andreasjevn@gmail.com>
2020-04-22 23:50:49 +0200

    Added lap timer and best lap tracking

---
+++ Assets/Scripts/HalfPointTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfPointTrigger : MonoBehaviour
+{
+
+    public GameObject LapTrigger;
+    public GameObject HalfLapTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        LapTrigger.SetActive(true);
+        HalfLapTrigger.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/LapComplete.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapComplete : MonoBehaviour
+{
+
+    public GameObject LapTrigger;
+    public GameObject HalfLapTrigger;
+
+    public GameObject MinuteDisplay;
+    public GameObject SecondDisplay;
+    public GameObject MilliDisplay;
+
+    public GameObject LapTimeBox;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(LapTimeManager.SecondCount <= 9)
+        {
+            SecondDisplay.GetComponent<TMPro.TextMeshProUGUI>().text = "0" + LapTimeManager.SecondCount + ".";
+        } else
+        {
+            SecondDisplay.GetComponent<TMPro.TextMeshProUGUI>().text = "" + LapTimeManager.SecondCount + ".";
+        }
+
+        if (LapTimeManager.MinuteCount <= 9)
+        {
+            MinuteDisplay.GetComponent<TMPro.TextMeshProUGUI>().text = "0" + LapTimeManager.MinuteCount + ".";
+        }
+        else
+        {
+            MinuteDisplay.GetComponent<TMPro.TextMeshProUGUI>().text = "" + LapTimeManager.MinuteCount + ".";
+        }
+
+        MilliDisplay.GetComponent<TMPro.TextMeshProUGUI>().text = "" + LapTimeManager.MilliCount;
+
+        LapTimeManager.MinuteCount = 0;
+        LapTimeManager.SecondCount = 0;
+        LapTimeManager.MilliCount = 0;
+
+        LapTrigger.SetActive(false);
+        HalfLapTrigger.SetActive(true);
+
+
+    }
+
+}
